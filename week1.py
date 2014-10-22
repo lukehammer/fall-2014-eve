50f0e91e38e428a9c245727ec6c1b333da2b4d6b
student <luke@lukehammermagic.com>
2014-10-21 21:16:20 -0700

    my first commit

---
+++ week1.py
@@ -0,0 +1,15 @@
+__author__ = 'student'
+first_name = "Luke"
+def print_name():
+    age = 33
+    first_name = "Luke"
+
+    #this is a comment
+    #print and str are built in functions
+    print(first_name + " is " + str(age))
+
+
+print (first_name + first_name)
+print (2+2)
+
+

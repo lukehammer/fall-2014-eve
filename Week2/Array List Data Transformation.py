d6ec9cc54d12e08b74eb18a0862f24263777c49a
lukehammer <luke@lukehammermagic.com>
2014-11-05 19:03:40 -0800

    code guild commit

--- Week2/Array List Data Transformation.py
+++ Week2/Array List Data Transformation.py
@@ -10,13 +10,39 @@ test_data = [
     ["2014-06-04", "MSFT", 40.71],
 ]
 
+master = {}
+for x in test_data:
+    ticker= x[1]
+    list = master.keys()
+    if ticker not in list:
+        master[ticker] = []
+    simple =[ x[0],x[2]]
+    master[ticker].append(simple)
 
 
-luke ="a cool guy"
+print master
 
-appl = "appl"
 
-if appl == "appl":
-    print True
-else:
-    print False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tickers = {}
+for stock in test_data:
+    symbol = stock[1]
+    if not symbol in tickers.keys():
+        tickers[symbol] = []
+    items = tickers[symbol]
+    items.append(stock)
